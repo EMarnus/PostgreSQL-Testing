c042b6e2df0fa0ef7eaad46852ee9d1082526c1a
EMarnus <>
2023-05-31 20:30:56 +0000

    Finalish commit

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -10,6 +10,9 @@ cursor = connection.cursor()
 # fetch the results (multiple)
 results = cursor.fetchall()
 
+# Query 1 - select all records from the "Artist" table
+cursor.execute('SELECT * FROM "Artist"')
+
 # fetch the result (single)
 # results = cursor.fetchone()
 
